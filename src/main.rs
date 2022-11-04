dc9a301eacc4a9f58eda5843ac5a47f933441b99
Andy Baizon <andrew.baizon@oneadvanced.com>
2022-11-04 11:12:35 +0000

    feat(main): lambdafication

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
 // imports
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_synthetics as synthetics;
-use lambda_runtime::Error;
-use std::panic;
+use lambda_runtime::{service_fn, LambdaEvent, Error};
+use serde_json::Value;
+//use std::panic;
 
 // main
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // aws client config
+
+    // fire the canary check
+    let handler = service_fn(check_canary);
+    lambda_runtime::run(handler).await?;
+    
+    // return ok
+    Ok(())
+
+}
+
+async fn check_canary (_event: LambdaEvent<Value>) -> Result<(), Error> {
+
+    // lambda environment variables
+    let region = std::env::var("canaryRegion").unwrap();
+    let canary = std::env::var("canaryName").unwrap();
+
+    // aws sdk client config
     let region_provider =
-        RegionProviderChain::default_provider().or_else(synthetics::Region::new("eu-west-2"));
+        RegionProviderChain::default_provider().or_else(synthetics::Region::new(region));
     let shared_config = aws_config::from_env().region(region_provider).load().await;
     let client = synthetics::Client::new(&shared_config);
 
     // pull the last canary run
     let mut canary_names = Vec::new();
-    canary_names.push(String::from("test-canary"));
+    canary_names.push(String::from(canary));
     let names: Option<Vec<String>> = Some(canary_names);
 
     let canary_runs = client
@@ -41,9 +58,11 @@ async fn main() -> Result<(), Error> {
 
     // this is a hacky bailout
     if state == "FAILED" {
+        println!("Bailing out");
         panic!();
     };
 
     // return ok
     Ok(())
+
 }
